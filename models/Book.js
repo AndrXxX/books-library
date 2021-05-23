68e6a07108997a9fa58b6de0f4d71373730cc57f
Andrew Garetov <andrew1gar@yandex.ru>
2021-05-23 15:15:03 +0300

    #006 Добавлена модель книги

---
+++ models/Book.js
@@ -0,0 +1,15 @@
+const uidGenerator = require('node-unique-id-generator');
+
+class Book {
+  constructor(title = "", desc = "") {
+    this.id = uidGenerator.generateUniqueId();
+    this.title = title;
+    this.description = desc;
+    this.authors = "";
+    this.favorite = "";
+    this.fileCover = "";
+    this.fileName = "";
+  }
+}
+
+module.exports = Book;
