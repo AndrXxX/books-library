b246812f6a0914c3fb2a3603a02a3a327ade1611
Andrew Garetov <andrew1gar@yandex.ru>
2021-05-29 17:09:21 +0300

    #007 Возможность загрузки файлов

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fileMiddleware = require('../middleware/file');
 
 const {Book} = require('../models');
 const bookUpdater = require('../services/BookUpdater')();
@@ -17,17 +18,24 @@ router.get('/', (req, res) => {
   res.json(books);
 });
 
-router.post('/', (req, res) => {
-  const {books} = store;
-  const {title, description} = req.body;
+router.post('/',
+  fileMiddleware.single('book-file'),
+  (req, res) => {
+    const {books} = store;
+    const {title, description} = req.body;
 
-  const newBook = new Book(title, description);
-  bookUpdater.updateByObject(newBook, req.body);
-  books.push(newBook);
+    const newBook = new Book(title, description);
+    bookUpdater.updateByObject(newBook, req.body);
+    if (req.file) {
+      const {path} = req.file;
+      newBook.fileBook = path;
+    }
+    books.push(newBook);
 
-  res.status(201);
-  res.json(newBook);
-});
+    res.status(201);
+    res.json(newBook);
+  }
+);
 
 router.get('/:id', (req, res) => {
   const {books} = store;
@@ -42,19 +50,26 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  const {books} = store;
-  const {id} = req.params;
-  const idx = books.findIndex(el => el.id === id);
+router.put('/:id',
+  fileMiddleware.single('book-file'),
+    (req, res) => {
+    const {books} = store;
+    const {id} = req.params;
+    const idx = books.findIndex(el => el.id === id);
 
-  if (idx !== -1) {
-    bookUpdater.updateByObject(books[idx], req.body);
-    res.json(books[idx]);
-  } else {
-    res.status(404);
-    res.json("book | not found");
+    if (idx !== -1) {
+      bookUpdater.updateByObject(books[idx], req.body);
+      if (req.file) {
+        const {path} = req.file;
+        books[idx].fileBook = path;
+      }
+      res.json(books[idx]);
+    } else {
+      res.status(404);
+      res.json("book | not found");
+    }
   }
-});
+);
 
 router.delete('/:id', (req, res) => {
   const {books} = store;
@@ -69,5 +84,27 @@ router.delete('/:id', (req, res) => {
     res.json("book | not found");
   }
 });
+
+router.post('/:id/upload-file',
+  fileMiddleware.single('book-file'),
+  (req, res) => {
+    if (!req.file) {
+      res.status(400);
+      res.json("file not uploaded");
+    }
+    const {books} = store;
+    const {id} = req.params;
+    const idx = books.findIndex(el => el.id === id);
+
+    if (idx !== -1) {
+      const {path} = req.file;
+      books[idx].fileBook = path;
+      res.json(books[idx]);
+    } else {
+      res.status(404);
+      res.json("book | not found");
+    }
+  }
+);
 
 module.exports = router;
