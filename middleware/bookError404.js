acb82e430093a56e0c8d7ac5996f52e8a738acc3
Andrew Garetov <andrew1gar@yandex.ru>
2021-05-29 17:56:27 +0300

    #007 Корректная работа middleware

--- middleware/bookError404.js
+++ middleware/bookError404.js
@@ -1,8 +1,9 @@
 module.exports = (store) => {
-  return (req, res) => {
+  return (req, res, next) => {
     if (!store.hasBook(req.params.id)) {
       res.status(404);
       return res.json("book | not found");
     }
+    return next();
   }
 };
