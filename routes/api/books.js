fa32faaa9bea5b1d4587b33bb70eb8fbd9c07c4b
Andrew Garetov <andrew1gar@yandex.ru>
2021-05-29 21:34:20 +0300

    #008 Вынос Store в отдельный файл

--- routes/api/books.js
+++ routes/api/books.js
@@ -6,30 +6,7 @@ const path = require('path');
 
 const {Book} = require('../../models');
 const bookUpdater = require('../../services/BookUpdater')();
-const store = {
-  books: [],
-  getIdx(id) {
-    return this.books.findIndex(el => el.id === id);
-  },
-  hasBook(id) {
-    return this.getIdx(id) !== -1;
-  },
-  findBook(id) {
-    return this.hasBook(id) ? this.books[this.getIdx(id)] : null;
-  },
-  deleteBook(id) {
-    if (!this.hasBook(id)) {
-      return false;
-    }
-    this.books.splice(this.getIdx(id), 1);
-    return true;
-  },
-};
-
-[1, 2, 3].map(el => {
-  const newBook = new Book(`book ${el}`, `desc book ${el}`);
-  store.books.push(newBook);
-});
+const store = require('../../services/Store');
 
 router.get('/', (req, res) => {
   const {books} = store;
