8e75b2962f07263d8709b9fed5ccf779514a00ee
Andrew Garetov <andrew1gar@yandex.ru>
2021-06-27 00:58:10 +0300

    #012 Правка путей загрузки файлов

--- ndse-library/app/middleware/file.js
+++ ndse-library/app/middleware/file.js
@@ -3,7 +3,11 @@ const MAX_FILE_SIZE = 10*1024*1024;
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'public/books')
+    if (file.fieldname === "fileBook") {
+      cb(null, 'public/books');
+    } else {
+      cb(null, 'public/images');
+    }
   },
   filename(req, file, cb) {
     cb(null, `${new Date().toISOString().replace(/:/g, '-')}-${file.originalname}`)
