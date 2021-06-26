6057052a2d1c2a525cf0215120060aa9c4b839b6
Andrew Garetov <andrew1gar@yandex.ru>
2021-06-26 20:01:35 +0300

    #012 Подключение passport

--- ndse-library/app/index.js
+++ ndse-library/app/index.js
@@ -1,21 +1,36 @@
 const express = require('express');
+const passport = require('passport')
+const expressSession = require('express-session')
 const cors = require('cors');
 const mongoose = require('mongoose');
+const auth = require('./boot/auth');
 
 const booksRouter = require('./routes/books');
+const userRouter = require('./routes/user');
 const apiBooksRouter = require('./routes/api/books');
 const apiUserRouter = require('./routes/api/user');
 const indexRouter = require('./routes/index');
 
 const loggerMiddleware = require('./middleware/logger');
+const authMiddleware = require('./middleware/auth');
 const errorMiddleware = require('./middleware/error404');
 
 const app = express();
+auth();
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+
+app.use(expressSession({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(cors());
 app.set("view engine", "ejs");
 app.use(loggerMiddleware);
@@ -23,9 +38,11 @@ app.use('/public', express.static(__dirname+"/public"));
 
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
+app.use('/user', userRouter);
 app.use('/api/books', apiBooksRouter);
 app.use('/api/user', apiUserRouter);
 
+app.use(authMiddleware);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
---
+++ ndse-library/app/routes/user.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const registerMiddleware = require('../middleware/register');
+const authMiddleware = require('../middleware/auth');
+
+router.get('/login',
+function (req, res) {
+    res.render('user/login', {
+      title: "Авторизация",
+    })
+  },
+);
+
+router.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/user/login',
+  }),
+);
+
+router.get('/register',
+  function (req, res) {
+    if (req.user) res.redirect('/');
+    res.render('user/register', {
+      title: "Регистрация",
+      user: {},
+      error: false,
+      info: false,
+    })
+  },
+);
+
+router.post('/register',
+  registerMiddleware,
+  function (req, res) {
+    res.render('user/register', {
+      title: "Регистрация",
+      user: req.body.user | {},
+      error: req.error,
+      info: req.info,
+    });
+  },
+
+);
+
+router.get('/logout',
+  function (req, res) {
+    req.logout()
+    res.redirect('/')
+  },
+);
+
+router.get('/profile',
+  authMiddleware,
+  function (req, res, next) {
+    if (!req.isAuthenticated || !req.isAuthenticated()) {
+      if (req.session) {
+        req.session.returnTo = req.originalUrl || req.url
+      }
+      return res.redirect('/login')
+    }
+    next()
+  },
+  function (req, res) {
+    res.render('profile', { user: req.user })
+  })
+
+module.exports = router;
