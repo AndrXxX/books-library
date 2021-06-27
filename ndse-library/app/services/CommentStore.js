d149e22d156299a35f8a271ed0c5d112f303bd9d
Andrew Garetov <andrew1gar@yandex.ru>
2021-06-27 20:08:00 +0300

    #013 Добавлена модель комментариев

---
+++ ndse-library/app/services/CommentStore.js
@@ -0,0 +1,22 @@
+const { Comment } = require('../models');
+
+const store = {
+  async find(limit, cb) {
+    try {
+      cb(null, await Comment.find().sort({'date': 1}).limit(limit).select('-__v'));
+    } catch (e) {
+      cb(e, null);
+    }
+  },
+  async create(params, cb) {
+    const comment = new Comment(params);
+    try {
+      await comment.save();
+      cb(null, comment);
+    } catch (e) {
+      cb(e, comment);
+    }
+  },
+};
+
+module.exports = store;
