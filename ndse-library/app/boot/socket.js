b34a55ba7f3a21cc0ecdb0a04ec8c211d6a4496f
Andrew Garetov <andrew1gar@yandex.ru>
2021-06-27 20:34:10 +0300

    #013 Сохранение сообщений в базу, загрузка первых сообщений

--- ndse-library/app/boot/socket.js
+++ ndse-library/app/boot/socket.js
@@ -1,11 +1,22 @@
+const commentStore = require('../services/CommentStore');
+
+const onLoadBookDiscussion = (socket) => {
+  commentStore.find(5, (err, comments) => {
+    comments && socket.emit('load-book-discussion', comments);
+  })
+}
+
 const onBookDiscussion = (socket) => {
   const {bookId} = socket.handshake.query;
+  onLoadBookDiscussion(socket);
+
   console.log(`Socket bookId: ${bookId}`);
   socket.join(bookId);
   socket.on('book-discussion', (msg) => {
-    msg.type = `bookId: ${bookId}`;
-    socket.to(bookId).emit('book-discussion', msg);
-    socket.emit('book-discussion', msg);
+    commentStore.create(msg, (err, comment) => {
+      socket.to(bookId).emit('book-discussion', comment);
+      socket.emit('book-discussion', comment);
+    });
   });
 }
 
@@ -15,7 +26,7 @@ const onDisconnect = (socket, id) => {
   });
 }
 
-module.exports = function(io) {
+module.exports = (io) => {
   io.on('connection', (socket) => {
     const {id} = socket;
     console.log(`Socket connected: ${id}`);
