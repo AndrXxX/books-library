8d545b859cb1791e0099dfacc370e7e2c516c1dc
Andrew Garetov <andrew1gar@yandex.ru>
2021-07-04 15:07:04 +0300

    #002 Адаптация TS в error404

--- ndse-library/app/middleware/error404.ts
+++ ndse-library/app/middleware/error404.ts
@@ -1,4 +1,6 @@
-export default (req, res) => {
+import { Request, Response } from "express";
+
+export default (req: Request, res: Response) => {
   res.render("error/404", {
     title: "404 | страница не найдена",
   });
