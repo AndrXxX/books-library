b833a7481923442c1c94c300db9887087c548281
Andrew Garetov <andrew1gar@yandex.ru>
2021-07-24 15:47:55 +0300

    #003 Использование интерфейса Book вместо any

--- ndse-library/app/services/BooksService.ts
+++ ndse-library/app/services/BooksService.ts
@@ -1,26 +1,27 @@
-import { injectable } from "inversify";
 import { BooksRepositoryInterface } from "../services/Interfaces/BooksRepositoryInterface";
+import { Book } from "../models/Book";
+import { injectable } from "inversify";
 
 @injectable()
 export class BooksService {
   constructor(private readonly repo: BooksRepositoryInterface) {}
 
-  getBooks(): Promise<any[]> {
+  getBooks(): Promise<Book[]> {
     return this.repo.getBooks();
   }
   hasBook(id: string): Promise<boolean> {
     return this.repo.hasBook(id);
   }
-  getBook(id: string): Promise<any | null> {
+  getBook(id: string): Promise<Book | null> {
     return this.repo.getBook(id);
   }
   deleteBook(id: string): Promise<boolean> {
     return this.repo.deleteBook(id);
   }
-  updateBook(id: string, params: any): Promise<boolean> {
+  updateBook(id: string, params: Book): Promise<boolean> {
     return this.repo.updateBook(id, params);
   }
-  createBook(params: any): Promise<any> {
+  createBook(params: Book): Promise<Book> {
     return this.repo.createBook(params);
   }
 }
--- ndse-library/app/services/Interfaces/BooksRepositoryInterface.ts
+++ ndse-library/app/services/Interfaces/BooksRepositoryInterface.ts
@@ -1,8 +1,10 @@
+import { Book } from "../../models/Book";
+
 export abstract class BooksRepositoryInterface {
-  abstract getBooks(): Promise<any[]>;
+  abstract getBooks(): Promise<Book[]>;
   abstract hasBook(id: string): Promise<boolean>;
-  abstract getBook(id: string): Promise<any | null>;
+  abstract getBook(id: string): Promise<Book | null>;
   abstract deleteBook(id: string): Promise<boolean>;
-  abstract updateBook(id: string, params: any): Promise<boolean>;
-  abstract createBook(params: any): Promise<any>;
+  abstract updateBook(id: string, params: Book): Promise<boolean>;
+  abstract createBook(params: Book): Promise<Book>;
 }
